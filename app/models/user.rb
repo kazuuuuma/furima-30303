f444f6b6e242e6f4d0261817befaba278c80c8d2
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2020-11-21 14:30:13 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :fam_name_furigana, presence: true
+  validates :fst_name_furigana, presence: true
+  validates :birthday, presence: true
 end
