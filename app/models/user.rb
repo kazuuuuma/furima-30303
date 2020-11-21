0e008150d348b35b316886d272bf731bc10c2e97
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2020-11-21 15:49:05 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: true
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
