4b71ff1c23a00e2e08230b2308ee1ef22724a84a
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2020-11-25 21:15:45 +0900

    バリデーション修正

--- Gemfile
+++ Gemfile
@@ -57,3 +57,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'pry-rails'
+group :development do
+  gem 'rubocop', require: false
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :fam_name_furigana
+    validates :fst_name_furigana
+  end
+
   validates :email, uniqueness: true
   validates :nickname, presence: true
-  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :fam_name_furigana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :fst_name_furigana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
   has_many :items
