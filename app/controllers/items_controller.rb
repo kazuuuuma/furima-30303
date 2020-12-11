28cbcfef65a4d6d74a71192ebf26d82221b6fe34
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2020-12-11 21:27:51 +0900

    商品機能詳細追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_sing_in, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -47,5 +48,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
 end
