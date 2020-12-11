2a289fc6ba927243d5447e3f48a4a04ae4b3eaa0
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2020-12-11 21:48:56 +0900

    商品編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_sing_in, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,15 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    item = Item.find(params[:id])
+    @item = Item.find(params[:id])
     if item.update(item_params)
       redirect_to item_path(item.id), method: :get
     else
@@ -53,5 +54,9 @@ class ItemsController < ApplicationController
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
